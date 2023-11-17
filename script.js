6e166305742a77b84db1b94210908cd3f72bb5a6
savageafrika <113559289+savageafrika@users.noreply.github.com>
2023-11-17 03:16:24 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,545 @@
+
+
+
+
+ 
+
+   //Questions array
+
+   let questions = [
+
+    {
+
+         number:1,
+
+         question: "___ are seen as building blocks to the sentence ?",
+
+         answer : "Phrases and clauses",
+
+         options : [
+
+                          "Grammar",
+
+                          "Sentence fragments",
+
+                          "Phrases and clauses",
+
+                          "Subject and predicate"
+
+                        ]
+
+    
+
+    },
+
+    {
+
+         number:2,
+
+         question: " The main unit of structure in English grammar is__",
+
+         answer : "Clause",
+
+         options : [
+
+                          "Phrase",
+
+                          "Clause",
+
+                          "Sentence",
+
+                          "Sounds"  
+
+                          ]
+
+    
+
+    },
+
+   {
+
+         number:3,
+
+         question: " __ is a group of words which make incomplete sense",
+
+         answer : "Phrase",
+
+         options : [ 
+
+                          "Phrase",
+
+                          "Clause",
+
+                          "Sentence fragments",
+
+                          "Syllabic formation"
+
+                          ]
+
+    
+
+    },
+
+    {
+
+         number:4,
+
+         question: "When a noun phrases referring to the same thing or person as a subject it serves as the___",
+
+         answer : "Complement",
+
+         options : [
+
+                          "Subject",
+
+                          "Appositive",
+
+                          "Complement",
+
+                          "Object"
+
+                          ]
+
+    }, 
+
+    {
+
+         number:5,
+
+         question: "__ has strict rules of order",
+
+         answer : "Verb phrase",
+
+         options : [ 
+
+                          "Verb phrase",
+
+                          "Prepositional phrase",
+
+                          "Adjectival phrase",
+
+                          "Adverbial phrase"
+
+                       ]
+
+    },
+
+    {
+
+         number:6,
+
+         question: "Phrases are classified according to __",
+
+         answer : "Class of word",
+
+         options : [
+
+                          "Syntactic categories",
+
+                          "Class of word",
+
+                          "Type of phrase",
+
+                          "Grammatical function"
+
+                       ]
+
+    },
+
+    {
+
+         number:7,
+
+         question: "The man at the gate is my father, the underlined group of words is a ___",
+
+         answer : "Noun phrase",
+
+         options : [
+
+                          "Noun phrase",
+
+                          "Adjectival phrase",
+
+                          "Prepositional phrase",
+
+                          "Adverbial phrase"
+
+                       ]
+
+    },
+
+     {
+
+         number:8,
+
+         question: "The book on religious studies is missing, the underlined group of words is a___",
+
+         answer : "Adjectival phrase",
+
+         options : [
+
+                         "Noun phrase",
+
+                         "Adjectival phrase",
+
+                         "Prepositional phrase",
+
+                         "Verb phrase"
+
+                       ]
+
+    },
+
+    {
+
+         number:9,
+
+         question: "The balloon rose off the ground , the underlined group of words is a ___",
+
+         answer : "Prepositional phrase",
+
+         options : [
+
+                         "Noun phrase",
+
+                         "Adjectival phrase",
+
+                         "Prepositional phrase",
+
+                         "Adverbial phrase"
+
+                       ]
+
+    },
+
+    {
+
+         number:10,
+
+         question: "The man kicked the bucket because of illness, the underlined group of words is a___",
+
+         answer : "Adverbial phrase",
+
+         options : [
+
+                         "Noun phrase",
+
+                         "Adjectival phrase",
+
+                         "Prepositional phrase",
+
+                         "Adverbial phrase"
+
+                       ]
+
+    },
+
+    {
+
+         number:11,
+
+         question: "a prepositional phrase modifies a noun or pronoun it acts as ___",
+
+         answer : "Adjective",
+
+         options : [
+
+                         "Adjunct",
+
+                         "Post modifier",
+
+                         "Adverbial",
+
+                         "Adjective"
+
+                       ]
+
+    },
+
+    {
+
+         number:12,
+
+         question: "Which of these is not part of the structure of a phrase",
+
+         answer : "Determiner",
+
+         options : [
+
+                         "Modifier",
+
+                         "Determiner",
+
+                         "Quantifier",
+
+                         "Head"
+
+                       ]
+
+    },
+
+    {
+
+         number:13,
+
+         question: "The most important item in a phrase is ___",
+
+         answer : "Head",
+
+         options : [
+
+                         "Modifier",
+
+                         "Determiner",
+
+                         "Quantifier",
+
+                         "Head"
+
+                       ]
+
+    },
+
+    {
+
+         number:14,
+
+         question: "A clause expresses an action or a ___",
+
+         answer : "State of being",
+
+         options : [
+
+                         "State of being",
+
+                         "Relationship",
+
+                         "Fragment",
+
+                         "Notion"
+
+                       ]
+
+    },
+
+    {
+
+         number:15,
+
+         question: "A group of words with a subject and predicate which cannot stand on its own is called",
+
+         answer : "Subordinate clause",
+
+         options : [
+
+                         "Noun phrase",
+
+                         "Subordinate clause",
+
+                         "Sentence fragment",
+
+                         "Appositive"
+
+                       ]
+
+    },
+
+    {
+
+         number:16,
+
+         question: "The functions of prepositional phrase does not include",
+
+         answer : "Adjective",
+
+         options : [
+
+                         "Post modifier",
+
+                         "Adjective",
+
+                         "Adjunct",
+
+                         "Conjunct"
+
+                       ]
+
+    },
+
+    {
+
+         number:17,
+
+         question: "Noun clauses are introduced by",
+
+         answer : "Subordinators",
+
+         options : [
+
+                         "Modifiers",
+
+                         "Determiner",
+
+                         "Quantifier",
+
+                         "Subordinators"
+
+                       ]
+
+    },
+
+    {
+
+         number:18,
+
+         question: "The subject complement follows a __",
+
+         answer : "Copular verb",
+
+         options : [
+
+                         "Copular verb",
+
+                         "Transitive verb",
+
+                         "Finite verb",
+
+                         "Verb phrase"
+
+                       ]
+
+    },
+
+    {
+
+         number:19,
+
+         question: "If a noun supports another word, phrase or clause by describing it, that noun is in ___",
+
+         answer : "Appositive",
+
+         options : [
+
+                         "Complement",
+
+                         "Adjunct",
+
+                         "Appositive",
+
+                         "Congruence"
+
+                       ]
+
+    },
+
+    {
+
+         number:20,
+
+         question: "Another name for adjectival clause is __",
+
+         answer : "Relative clause",
+
+         options : [
+
+                         "Subordinate clause",
+
+                         "Relative clause",
+
+                         "post modifier",
+
+                         "Quantifier"
+
+                       ]
+
+    },
+
+    {
+
+         number:21,
+
+         question: "What he said was recorded on tape, the underlined group of words is",
+
+         answer : "Noun clause",
+
+         options : [
+
+                         "Noun clause",
+
+                         "Adverbial clause",
+
+                         "Prepositional clause",
+
+                         "adjectival clause"
+
+                       ]
+
+    },
+
+    {
+
+         number:22,
+
+         question: "The house I told you about has been demolished, the underlined group of words is",
+
+         answer : "adjectival clause",
+
+         options : [
+
+                         "Noun clause",
+
+                         "Adverbial clause",
+
+                         "Prepositional clause",
+
+                         "adjectival clause"
+
+                       ]
+
+    },
+
+    {
+
+         number:23,
+
+         question: "I always like to study where there is no noise. , The underlined group of words is",
+
+         answer : "Adverbial clause",
+
+         options : [
+
+                         "Noun clause",
+
+                         "Adverbial clause",
+
+                         "Prepositional clause",
+
+                         "adjectival clause"
+
+                       ]
+
+    },
+
+    {
+
+         number:24,
+
+         question: "Which one is not a type of subordinate clause",
+
+         answer : "Prepositional clause",
+
+         options : [
+
+                         "Noun clause",
+
+                         "Adverbial clause",
+
+                         "Prepositional clause",
+
+                         "adjectival clause"
+
+                       ]
+
+    }
+
+     ];    
